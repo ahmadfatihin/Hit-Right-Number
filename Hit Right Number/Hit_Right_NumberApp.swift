1c2ba3c9412751553ebb7249a32d0a20be9c9f9e
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2023-02-07 09:39:10 +0700

    Initial commit

---
+++ Hit Right Number/Hit_Right_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hit_Right_NumberApp.swift
+//  Hit Right Number
+//
+//  Created by Prima Interaktif on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Hit_Right_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
