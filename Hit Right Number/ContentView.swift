52e091f5c6f42c14eca21f72087375b04d548ea2
Prima Interaktif - Developer <ahmdfatihin17@gmail.com>
2023-02-11 23:56:21 +0700

    feat: update first screen

--- Hit Right Number/ContentView.swift
+++ Hit Right Number/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Hit Right Number
 //
-//  Created by Prima Interaktif on 06/02/23.
+//  Created by Ahmad Fatihin on 06/02/23.
 //
 
 import SwiftUI
@@ -10,10 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+            Text("🎯🎯🎯\n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO").multilineTextAlignment(.center)
+            Text("89")
+            HStack {
+                Text("1")
+                Slider( value: .constant(89), in:1.0...100.0)
+                Text("100")
+            }
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
         }
         .padding()
     }
@@ -24,3 +31,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
